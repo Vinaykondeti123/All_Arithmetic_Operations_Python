3f22ea1fa094fca1452d6fa410b74aad8d87ca7d
Vinay Kondeti <vinaykondeti1998@gmail.com>
2024-11-16 20:20:38 +0530

    Add files via upload

---
+++ All_Arithmetic_Operations_Python.py
@@ -0,0 +1,8 @@
+# performing all arithmetic operations
+a=float(input('Enter first number: '))
+b=float(input('Enter second number: '))
+print(f'Sum: {a+b}')
+print(f'Subtraction: {a-b}')
+print(f'Multiplication: {a*b}')
+print(f'Division: {a/b}')
+print(f'Remainder: {a%b}')
